12b56e95484c13629787433323401cdb1f3139a9
zaidkhansahab <kzaid6008@gmail.com>
2025-03-10 09:32:05 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/style/mixins.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  }
+});
